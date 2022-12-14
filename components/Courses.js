491e365e76039886cf2b893d8be59dc5717cf968
thejas <thejaskala308@gmail.com>
2022-12-14 19:14:58 +0530

    make it better

--- components/Courses.js
+++ components/Courses.js
@@ -13,11 +13,11 @@ export default function Courses() {
 		transition={{duration:1.5}}
 		>
 			<h3 className="uppercase tracking-[20px] text-gray-500 text-2xl" >
-				M-V-V
+				Mission-vision
 			</h3>
 
 		</motion.div>
-		<div className="grid grid-cols-1 md:grid-cols-3 w-full h-full md:mt-[200px] mt-10 px-5 py-5 gap-10" >
+		<div className="grid grid-cols-1 md:grid-cols-2 w-full h-full md:mt-[200px] mt-10 px-5 py-5 gap-10" >
 			<motion.div
 			initial={{
 				opacity:0.5
@@ -57,25 +57,7 @@ export default function Courses() {
 				</div>
 
 			</motion.div>
-			<motion.div
-			initial={{
-				opacity:0.5
-			}}
-			whileInView={{
-				opacity:1
-			}}
-			transition={{
-				duration:1.5
-			}}
-			className="rounded-xl bg-gray-700/40 p-5 h-[300px]"
-			>
-				<div className="flex flex-col gap-5 text-center justify-center" >	
-					<h1 className="text-3xl text-yellow-500 font-bold uppercase">V A L U E</h1>
-					<p className="text-lg text-gray-300/70 font-semibold">The world steps aside for the man who knows where he is going</p>
-
-				</div>
-
-			</motion.div>
+			
 
 
 		</div>
